6826d7217c0f60a4a1b47405568c284500f71fe8
Laya Rajaram <layarajaram@Layas-MacBook-Pro.local>
2023-12-26 15:33:27 -0600

    Add model files

---
+++ models/post.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const postSchema = new Schema({
+title: String,
+price: String,
+description: String,
+images: [ String ],
+location: String,
+lat: Number,
+lng: Number,
+author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+},
+reviews: [
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Review'
+    }
+]
+
+});
+
+module.exports = mongoose.model('Post', postSchema);
+
+
+
+
+
+
+
+
+/* 
+Post 
+title - string
+price - string 
+description - string
+images - array of strings
+location - string
+lat - int/float 
+lng - int/float
+author - Object id (ref to User)
+reviews - array of objects
+*/
